300e568679dc4bd28d32b84bdd6413e9ea293e60
Mahmoud <mahmoudelsharkawey52@gmail.com>
2024-05-10 10:45:21 +0300

    link js css feathericon

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+</head>
+<body>
+    Users App
+    <i data-feather="user-plus"></i>
+
+
+<script src="resources/js/bootstrap.min.js"></script>
+<script src="resources/js/icons.js"></script>
+<script>
+  feather.replace();
+</script>
+</body>
+</html>
